74b375fefb74a01ec17189d2bec8179b0b96bcbd
Shamim Reza <wpshamim@gmail.com>
2022-03-18 15:24:50 +0600

    add object and function

---
+++ function-multiparam.js
@@ -0,0 +1,29 @@
+function addTwoNum (_num1, _num2){
+    let add = _num1 + _num2;
+
+    return add;
+}
+
+multiplyTwoNumbers = (num1, num2) => {
+    let multiply = (num1 * num2);
+
+    return multiply;
+}
+
+divideTwoNum = (num1, num2) => {
+    let divide = num1 / num2;
+
+    return divide;
+}
+
+function subtract(num1, num2) {
+    let sub = num1 - num2;
+
+    return sub;
+}
+
+
+var result = multiplyTwoNumbers(25, 89);
+
+console.log(result);
+
---
+++ function-object.js
@@ -0,0 +1,7 @@
+function funcName(x) {
+    var pow = x*x;
+    return pow;
+}
+
+var result = funcName(20);
+console.log(res);
